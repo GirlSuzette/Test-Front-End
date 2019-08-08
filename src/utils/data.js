45d223e8e8729eaa98265ac7f9959b9f9910ad21
GirlSuzette <trinitysuzette_marpis@hotmail.com>
2019-08-07 23:09:35 -0500

    add data message

---
+++ src/utils/data.js
@@ -0,0 +1,72 @@
+export const messagesParteOne = [
+    {
+        "user": {
+            "name": "Roboto Regular16- #000000",
+            "username": "Roboto Light 14 - #797979",
+            "image": "bender",
+            "id": 1
+        },
+        "receiver_id": 2,
+        "message": "Roboto Light 15 - #000000"
+    },
+    {
+        "user": {
+            "name": "Roboto Regular16- #000000",
+            "username": "Roboto Light 14 - #797979",
+            "image": "pickle_rick",
+        },
+        "receiver_id": 1,
+        "message": "Roboto Light 15 - #000000"
+    }
+]
+
+export const messagesParteTwo = [
+    {
+        "user": {
+            "name": "Roboto Regular16- #000000",
+            "username": "Roboto Light 14 - #797979",
+            "image": "bender",
+            "id": 1
+        },
+        "receiver_id": 2,
+        "message": "Roboto Light 15 - #000000"
+    },
+    {
+        "user": {
+            "name": "Roboto Regular16- #000000",
+            "username": "Roboto Light 14 - #797979",
+            "image": "pickle_rick",
+        },
+        "receiver_id": 1,
+        "message": "Roboto Light 15 - #000000"
+    },
+    {
+        "user": {
+            "name": "Roboto Regular16- #000000",
+            "username": "Roboto Light 14 - #797979",
+            "image": "bender",
+            "id": 1
+        },
+        "receiver_id": 2,
+        "message": "Roboto Light 15 - #000000"
+    },
+    {
+        "user": {
+            "name": "Roboto Regular16- #000000",
+            "username": "Roboto Light 14 - #797979",
+            "image": "pickle_rick",
+        },
+        "receiver_id": 1,
+        "message": "Roboto Light 15 - #000000"
+    },
+    {
+        "user": {
+            "name": "Roboto Regular16- #000000",
+            "username": "Roboto Light 14 - #797979",
+            "image": "bender",
+            "id": 1
+        },
+        "receiver_id": 2,
+        "message": "Roboto Light 15 - #000000"
+    },
+]
